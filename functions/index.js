e8e65bb2e7877dfe91d2b1b55aa10fdd164901c2
unknown <simrangupta172002@gmail.com>
2023-07-20 18:52:57 +0530

    added payment method

--- functions/index.js
+++ functions/index.js
@@ -2,14 +2,13 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51N5KscSBXO05ZdayrfirWJPXSU2vpa7h3LH6dP9mEBW3VgJbNzpIneVrn7GbzPLZRNY8oy42DbeuaUHGrKjOJxtn00BukuwSC7"
-);
-//App config
+    "sk_test_51N5KscSBXO05ZdayrfirWJPXSU2vpa7h3LH6dP9mEBW3VgJbNzpIneVrn7GbzPLZRNY8oy42DbeuaUHGrKjOJxtn00BukuwSC7");
+//  App config
 const app = express();
-//Middlewares
+//  Middlewares
 app.use(cors({ origin: true }));
 app.use(express.json());
-//API routes
+//  API routes
 app.get("/", (request, response) => response.status(200).send("hello world"));
 app.post(`/payments/create`, async (request, response) => {
   const total = request.query.total;
@@ -17,7 +16,7 @@ app.post(`/payments/create`, async (request, response) => {
   console.log("payment request received", total);
 
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: total, //subunits of currency
+    amount: total, // subunits of currency
     currency: "usd",
   });
   // ok - created
@@ -26,5 +25,5 @@ app.post(`/payments/create`, async (request, response) => {
   });
 });
 exports.api = functions.https.onRequest(app);
-//Example endpoint
-//http://127.0.0.1:5001/clone-e2310/us-central1/api
+//  Example endpoint
+//  http://127.0.0.1:5001/clone-e2310/us-central1/api
